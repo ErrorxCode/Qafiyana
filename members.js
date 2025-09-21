260d8206ec6498314a2de94bbba528123eea9a74
Rahil <65817230+ErrorxCode@users.noreply.github.com>
2025-09-22 02:20:37 +0530

    Qafiyana 2.0

---
+++ members.js
@@ -0,0 +1,26 @@
+const admins = [
+    { name: 'Rahil', instaHandle: 'x1.rahil', tag: 'Owner' },
+    { name: 'Ashhar', instaHandle: 'x0.ash__', tag : 'Co-Owner' },
+    { name: 'Noor', instaHandle: 'nxoor.__', tag: 'Moderator' }
+];
+
+const members = [
+    { name: 'Arbaz', instaHandle: 'ak._7h' },
+    { name: 'Rashid', instaHandle: '_rxshid__11' , tag : 'Bestie'},
+    { name: 'Altamsh', instaHandle: 'altamash____khan____' , tag : 'Bro'},
+    { name: 'Surbhi', instaHandle: 'drama_girl_9998' },
+    { name: 'Liaana', instaHandle: 'kyroptozz_' ,tag : 'Kido'},
+    { name: 'Adeel', instaHandle: 'a_d_e_e_l___18' },
+    { name: 'Fatema', instaHandle: 'moon__veil0' },
+    { name: 'Noorain', instaHandle: 'quran0.114', tag : 'Loyal'},
+    { name: 'Sana', instaHandle: 'binte_raza987' ,tag : 'Wierdo'},
+    { name: 'Tabish', instaHandle: 'laaststreet', tag : 'Editor' },
+    { name: 'Aaliya', instaHandle: '_i.vaishnav.i_' , tag : 'Lawyer'},
+    { name: 'Sohail', instaHandle: 'x._aladin_.x'},
+    { name: 'Zainab', instaHandle: 'it_me_queen00o2' },
+    { name: 'Bilal', instaHandle: 'bilalkhalil3' },
+    { name: 'Zaman', instaHandle: 'shaahezaman_' },
+    { name: 'Fatima', instaHandle: 'chatpatygolgapyy' , tag : 'Doctor'},
+    { name: 'Taskeef', instaHandle: 'taskeef_kaisar'}
+];
+
